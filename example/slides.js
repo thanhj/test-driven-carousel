00d3c661d6bedf1988ca67d7bd3f9063a081179b
Thanh Nguyen Ky <thanh.nguyenky@singtel.com>
2019-08-25 18:23:29 +0800

    :bulb: Add example page

---
+++ example/slides.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const referralParams = 'utm_source=test-driven-carousel&utm_medium=referral';
+
+const getUsernameUrl = username =>
+  `https://unsplash.com/@${username}?${referralParams}`;
+
+const getAttribution = ({ name, username }) => (
+  <>
+    Photo by <a href={getUsernameUrl(username)}>{name}</a> on{' '}
+    <a href={`https://unsplash.com/?${referralParams}`}>Unsplash</a>
+  </>
+);
+
+export default [
+  {
+    description: 'Seattle',
+    attribution: getAttribution({
+      name: 'Ganapathy Kumar',
+      username: 'gkumar2175',
+    }),
+    imgUrl:
+      'https://images.unsplash.com/photo-1566590550646-fd9feb942d49?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=1084',
+  },
+  {
+    description: 'Chicago',
+    attribution: getAttribution({
+      name: 'Austin Neill',
+      username: 'arstyy',
+    }),
+    imgUrl:
+      'https://images.unsplash.com/photo-1564657536900-e6118c5e25f0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=1084',
+  },
+  {
+    description: 'Barcelona',
+    attribution: getAttribution({
+      name: 'Enes',
+      username: 'royalfound',
+    }),
+    imgUrl:
+      'https://images.unsplash.com/photo-1564586302016-48ce89f1d350?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=1084',
+  },
+  {
+    description: 'New York',
+    attribution: getAttribution({
+      name: 'Anthony DELANOIX',
+      username: 'anthonydelanoix',
+    }),
+    imgUrl:
+      'https://images.unsplash.com/photo-1566632210731-fe314e49a75f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=1084',
+  },
+  {
+    description: 'Rio de Janeiro',
+    attribution: getAttribution({
+      name: 'Agustín Diaz',
+      username: 'agussdiaz28',
+    }),
+    imgUrl:
+      'https://images.unsplash.com/photo-1565504052809-fd18286e5ed8?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=400&ixlib=rb-1.2.1&q=80&w=1084',
+  },
+];
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 // webpack.config.js
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development', //'production'
   entry: {
     casousel: './src/Carousel.js',
+    example: './example/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Carousel Example',
+      chunks: ['example'],
+    }),
+  ],
   module: {
     rules: [
       {
